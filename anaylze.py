254b06071626e032181772aae7d9806bd711dc55
zain08816 <zain08816@gmail.com>
2019-09-07 03:16:03 -0400

    Add natural language processing

--- anaylze.py
+++ anaylze.py
@@ -1,9 +1,45 @@
+import statistics
+
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+# Instantiates a client
+client = language.LanguageServiceClient()
+
+
 tweets = []
 f = open("tweets.txt", "r")
 for x in f:
     tweet = x[x.find('>')+2:len(x)-1]
     date = x[20:30]
-    tweets.append([date,tweet])
+    tweets.append([date,tweet.lower()])
+
+sentimentscore = []
+magnitudescore = []
+
+
+# The text to analyze
+for text in tweets:
+    text = text[1]
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+    print()
+    print('Text: {}'.format(text))
+    print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+    sentimentscore.append(sentiment.score)
+    magnitudescore.append(sentiment.magnitude)
+
+print(tweets)
+print(statistics.mean(sentimentscore))
+print(statistics.mean(magnitudescore))
+
+# flagged_words = ['kill', 'gun', 'dead', 'suicide', 'shoot', 'hurt', 'depressed']
 
 # dates = []
 # for i in range(len(tweets)):
