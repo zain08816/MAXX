4ff77e387ac48efa9074876648a8f04b0372f37f
Kirtan Patel <kirtanpatl@gmail.com>
2019-09-07 01:41:06 -0400

    Add an alternative method

--- anaylze.py
+++ anaylze.py
@@ -1,7 +1,17 @@
 tweets = []
 f = open("tweets.txt", "r")
 for x in f:
-    #print(x.find('>'))
     tweet = x[x.find('>')+2:len(x)-1]
     date = x[20:30]
     tweets.append([date,tweet])
+
+# dates = []
+# for i in range(len(tweets)):
+#     tweets[i] = tweets[i].split(' ')
+#     dates.append(tweets[i][1])
+#     for _ in range(5):
+#         tweets[i].pop(0)
+
+#     tweets[i] = ' '.join(tweets[i])
+
+# tweets_and_dates = zip(dates, tweets)
