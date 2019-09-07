f5a5a4e65a66c245256caa50526744b11e3d863b
zain08816 <zain08816@gmail.com>
2019-09-07 15:36:11 -0400

    add graph function and fix date issues

--- analyze.py
+++ analyze.py
@@ -1,4 +1,8 @@
 import statistics
+import pandas as pd
+from bokeh.io import output_file, show
+from bokeh.layouts import row
+from bokeh.plotting import figure
 
 # Imports the Google Cloud client library
 from google.cloud import language
@@ -12,9 +16,21 @@ client = language.LanguageServiceClient()
 tweets = []
 f = open("tweets.txt", "r")
 for x in f:
-    tweet = x[x.find('>')+2:len(x)-1]
-    date = x[20:30]
-    tweets.append([date,tweet.lower()])
+    x = x.split(' ')
+    date = ' '.join(x[1:3])
+    tweet = ' '.join(x[5:])
+    tweets.append([date, tweet])
+
+print(tweets)    
+    
+    
+     #= x[x.find('>')+2:len(x)-1]
+    # date = x[20:39]
+    # tweets.append([date,tweet.lower()])
+
+
+
+# tweets_and_dates = zip(dates, tweets)
 
 sentimentscore = []
 magnitudescore = []
@@ -35,18 +51,39 @@ for text in tweets:
     sentimentscore.append(sentiment.score)
     magnitudescore.append(sentiment.magnitude)
 
-print(statistics.mean(sentimentscore))
-print(statistics.mean(magnitudescore))
+
+weighted_sentiment = [sentimentscore[i]*magnitudescore[i] for i in range(len(magnitudescore))]
+dates = [data[0] for data in tweets]
+
+print(dates)
+print(weighted_sentiment)
+
+for i, date in enumerate(dates):
+    dates[i] = pd.to_datetime(date, format = "%Y-%m-%d %H:%M:%S")
+
+print(dates)
+x = dates
+y = weighted_sentiment
+
+# create graph
+
+# output to static HTML file
+output_file("lines.html")
+
+# create a new plot with a title and axis labels
+p = figure(title="Weighted Sentiment Over Time", x_axis_label='Date-Time', y_axis_label='Weighted Sentiment', x_axis_type="datetime")
+
+# add a line renderer with legend and line thickness
+p.line(x, y, legend="Tweet Sentiment.", line_width=2)
+# show the results
+show(p)
+
+
+
+
+
+# print(statistics.mean(sentimentscore))
+# print(statistics.mean(magnitudescore))
 
 # flagged_words = ['kill', 'gun', 'dead', 'suicide', 'shoot', 'hurt', 'depressed']
 
-# dates = []
-# for i in range(len(tweets)):
-#     tweets[i] = tweets[i].split(' ')
-#     dates.append(tweets[i][1])
-#     for _ in range(5):
-#         tweets[i].pop(0)
-
-#     tweets[i] = ' '.join(tweets[i])
-
-# tweets_and_dates = zip(dates, tweets)
