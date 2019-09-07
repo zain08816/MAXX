ba0ac604900233c887b0650e25e0ab071b04edbc
Kirtan Patel <kirtanpatl@gmail.com>
2019-09-06 23:47:39 -0400

    Change json format to txt

--- scraper.py
+++ scraper.py
@@ -7,10 +7,9 @@ c = twint.Config()
 
 c.Username = sys.argv[1]
 c.Limit = 20
-c.Store_json = True
-c.Output = "tweets"
+c.Output = "tweets.txt"
 
-if os.path.exists("tweets/tweets.json"):
-    shutil.rmtree("tweets")
+# if os.path.exists("tweets/tweets.json"):
+#     shutil.rmtree("tweets")
 
 twint.run.Search(c)
