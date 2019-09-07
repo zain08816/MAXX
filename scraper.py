6ebb4ebda3c1f966b0d218522d258c0ee77a4879
Kirtan Patel <kirtanpatl@gmail.com>
2019-09-06 23:18:14 -0400

    Remove folder before running scrapper

--- scraper.py
+++ scraper.py
@@ -1,11 +1,13 @@
 import twint
 import sys
+import shutil
 
 c = twint.Config()
 
 c.Username = sys.argv[1]
-c.Limit = 100
+c.Limit = 20
 c.Store_json = True
 c.Output = "tweets"
+shutil.rmtree("tweets")
 
 twint.run.Search(c)
