587ff81dbc31313978bfac9b83453cf097fc4cb3
zain08816 <zain08816@gmail.com>
2019-09-07 23:20:53 -0400

    add name formatting, better average analysis, and graph analysis

--- analyze.py
+++ analyze.py
@@ -4,7 +4,7 @@ import numpy as np
 
 #bokeh
 from bokeh.io import output_file, show
-from bokeh.layouts import row, gridplot, grid, column
+from bokeh.layouts import row, gridplot, grid, column, layout
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
 
@@ -19,13 +19,20 @@ client = language.LanguageServiceClient()
 
 tweets = []
 f = open("tweets.txt", "r")
+name = ''
 for x in f:
     x = x.split(' ')
     date = ' '.join(x[1:3])
     tweet = ' '.join(x[5:])
+    name = x[4]
     tweets.append([date, tweet])
 
-print(tweets)    
+# formatting name
+name = name[1:len(name)-1]
+name = '@'+name
+
+print(name)
+# print(tweets)    
     
     
      #= x[x.find('>')+2:len(x)-1]
@@ -52,6 +59,10 @@ for text in tweets:
     sentimentscore.append(sentiment.score)
     magnitudescore.append(sentiment.magnitude)
 
+# print(statistics.mean(sentimentscore))
+# print(statistics.mean(magnitudescore))
+
+
 #create the weitghted sentiment scores
 weighted_sentiment = [sentimentscore[i]*magnitudescore[i] for i in range(len(magnitudescore))]
 dates = [data[0] for data in tweets]
@@ -75,7 +86,9 @@ for i, sent in enumerate(weighted_sentiment):
 print(y)
 
 #find average sentiment
-average = abs(((max(y)*min(y))/2))
+average = statistics.mean(y)
+half = abs(average/2)
+
 print(average)
 
 #get outliers
@@ -84,10 +97,10 @@ positive_outliers_x = []
 negative_outliers_y = []
 negative_outliers_x = []
 for i, sent in enumerate(y):
-    if sent >= average:
+    if sent > average+half:
         positive_outliers_y.append(sent)
         positive_outliers_x.append(x[i])
-    if sent <= average*-1:
+    if sent < average-half:
         negative_outliers_y.append(sent)
         negative_outliers_x.append(x[i])
 
@@ -95,41 +108,52 @@ for i, sent in enumerate(y):
 output_file("lines.html")
 
 # create a new plot with a title and axis labels
-p = figure(plot_width = 1200, plot_height = 400, title="Weighted Sentiment Over Time", x_axis_label='Date-Time', y_axis_label='Weighted Sentiment', x_axis_type="datetime")
+
+top_title = "Weighted Sentiment Over Time For User: {}".format(name)
+p = figure(plot_width = 1200, plot_height = 400, title=top_title, x_axis_label='Date-Time', y_axis_label='Weighted Sentiment', x_axis_type="datetime")
 
 #plot average range
-p.line(x = [min(x),max(x)], y = [average, average], line_width = 1)
-p.line(x = [min(x),max(x)], y = [average*-1, average*-1], line_width = 1)
+p.line(x = [min(x),max(x)], y = [average+half, average+half], line_width = 1)
+p.line(x = [min(x),max(x)], y = [average, average], legend = 'Average Line', line_width = 2, color = 'black')
+p.line(x = [min(x),max(x)], y = [average-half, average-half], line_width = 1)
 
 #plot graph circles
-p.circle(x, y, legend="Tweet Sentiment.", size = 7, color="purple")
-p.circle(positive_outliers_x, positive_outliers_y, legend="Positive Sentiment.", size = 10, color="green")
-p.circle(negative_outliers_x, negative_outliers_y, legend="Negative Sentiment.", size = 10, color="red")
+p.circle(x, y, legend="Tweet Sentiment", size = 7, color="purple")
+p.circle(positive_outliers_x, positive_outliers_y, size = 10, color="green")
+p.circle(negative_outliers_x, negative_outliers_y, size = 10, color="red")
 p.line(x, y, line_width = 1,  color = "pink")
 
 #change total sentiment colors
 total_sentiment = sum(y)
 if total_sentiment > average:
     bar_color = "green"
-elif total_sentiment < average*-1:
+elif total_sentiment < average:
     bar_color = "red"
 else:
     bar_color = "grey"
 
 total = figure(plot_width = 200, plot_height = 400, title = "Total Sentiment")
-
 total.vbar(x = [1], width = 0.25, bottom = 0, top = [total_sentiment], color = bar_color)
 
+compare = figure(plot_width = 200, plot_height = 200, title = "Negative Outliers vs Positive Outliers")
+compare.vbar(x = [0], width = 0.25, bottom = 0, top = [len(negative_outliers_y)], color = 'red')
+compare.vbar(x = [0.3], width = 0.25, bottom = 0, top = [len(positive_outliers_y)], color = 'green')
+
+average = figure(plot_width = 200, plot_height = 200, title = "Average Sentiment and Average Magnitude")
+average.vbar(x = [0], width = 0.25, bottom = 0, top = [statistics.mean(sentimentscore)], color = 'blue')
+average.vbar(x = [0.3], width = 0.25, bottom = 0, top = [statistics.mean(magnitudescore)], color = 'purple')
 
 
 #show graphs
-show(column(p, total))
+l = layout([
+    [p],
+    [total, compare, average]
+], sizing_mode = 'stretch_both')
+show(l)
 
 
 
 
 
-# print(statistics.mean(sentimentscore))
-# print(statistics.mean(magnitudescore))
 
 
